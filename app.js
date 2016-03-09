ab6174a895826599a4ebadb9e94f207a58a05818
Marcin aka Sin <marcinmu330@gmail.com>
2016-03-08 19:56:57 -0600

    made updates to the module so you can initiate 'download', 'unzip' or 'convert' instead of doing all of them at once.

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 'use strict';
 // console.dir(process.argv);
-/* Stores URLs in an array. */
-var urls = process.argv.slice(2);
+var perform = process.argv[2];
 /* Includes download functionality */
-var link = require('./lib/download');
-/* Iterates and downloads each link */
-urls.forEach(link.get);
+var download = require('./lib/download');
+/* Includes unzip functionality */
+var file = require('./lib/unzip');
+/* Stores URLs in an array. */
+var urls;
+
+if(perform === 'download' || perform === 'unzip' || perform === 'convert'){
+
+	urls = process.argv.slice(3);
+	/* Iterates and downloads each link */
+	if(perform === 'download') {
+		urls.forEach(download.get);
+	}
+
+	if(perform === 'unzip') {
+		file.unzip();
+		console.log("Unzip me!");
+	}
+
+	if(perform === 'convert') {
+		/* Need to include this functionality. */
+	}
+
+} else {
+	/* need to add async functionality to allow the files to download, unzip and comvert */	
+	urls = process.argv.slice(2);
+	urls.forEach(download.get);
+	file.unzip();
+	/* need to include converting functionality */
+}
